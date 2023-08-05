a3960092da88bb465dc92ff06484176b65f2a4e6
Gurpartap <gurpartap6125@gmail.com>
2023-08-05 14:38:18 +0530

    video_3(exporting the obj data to be passed as a prop from another file)

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 import './App.css';
 import Video from './components/Video';
+import videos from './Data/data';
 
 function App() { 
 
   // here we are passing the obj as a prop to the video component
-  let videos=[
-    {
-      id:1,
-    title:"PHONE HACKING",
-    views:"345k",
-    time:"1 month",
-    channel:'Legend_Coder',
-    verified:true
-  },
-  {
-    id:2,
-  title:"LAPTOP HACKING",
-  views:"905k",
-  time:"4 month",
-  channel:'Legend_Coder',
-  verified:false
-},
-{
-  id:3,
-title:"WIFI HACKING",
-views:"1M",
-time:"9 month",
-channel:'Legend_Coder',
-verified:true
-}]
+//   let videos=[
+//     {
+//       id:1,
+//     title:"PHONE HACKING",
+//     views:"345k",
+//     time:"1 month",
+//     channel:'Legend_Coder',
+//     verified:true
+//   },
+//   {
+//     id:2,
+//   title:"LAPTOP HACKING",
+//   views:"905k",
+//   time:"4 month",
+//   channel:'Legend_Coder',
+//   verified:false
+// },
+// {
+//   id:3,
+// title:"WIFI HACKING",
+// views:"1M",
+// time:"9 month",
+// channel:'Legend_Coder',
+// verified:true
+// }]
  
   return (
   <>
@@ -36,6 +37,7 @@ verified:true
    {/* below we are using the object and the spread operator to pass the props   */}
    {videos.map(video=>(
     <Video
+      key={video.id}
       title={video.title}
       views={video.views}
       time={video.time}
---
+++ src/Data/data.js
@@ -0,0 +1,27 @@
+let videos=[
+    {
+    id:1,
+    title:"PHONE HACKING",
+    views:"345k",
+    time:"1 month",
+    channel:'Legend_Coder',
+    verified:true
+  },
+  {
+    id:2,
+  title:"LAPTOP HACKING",
+  views:"905k",
+  time:"4 month",
+  channel:'Legend_Coder',
+  verified:false
+},
+{
+  id:3,
+title:"WIFI HACKING",
+views:"1M",
+time:"9 month",
+channel:'Legend_Coder',
+verified:true
+}]
+
+export default videos
